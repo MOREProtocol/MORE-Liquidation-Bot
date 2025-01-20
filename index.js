d0ea9c375b020af197c055f96732433bf2885f11
mmsys34 <leon@more.markets>
2025-01-20 15:40:27 +0700

    feat: update code with graph integration

--- index.js
+++ index.js
@@ -11,13 +11,23 @@ const {
   Contract,
 } = require("ethers");
 
-const { positionsQuery } = require("./query.js");
+const { usersQuery } = require("./query.js");
 
 const config = require("./config.json");
+const PoolAbi = require("./abis/Pool.json");
 const OracleAbi = require("./abis/OracleAbi.json");
+const MulticallAbi = require("./abis/MulticallAbi.json");
 
 const provider = new providers.JsonRpcProvider(config.rpc_url);
-const oraclePriceScale = constants.WeiPerEther.mul(constants.WeiPerEther); // 1e36
+
+// interfaces
+const poolInterface = new utils.Interface(PoolAbi);
+
+const multicallContract = new Contract(
+  config.contracts.multicall,
+  MulticallAbi,
+  provider
+);
 
 const apolloFetcher = async (query) => {
   const client = new ApolloClient({
@@ -43,87 +53,114 @@ async function main() {
   let priceInfos = [];
   let unhealthPositions = [];
 
-  // fetch positions
-  const accountsInfo = await apolloFetcher(positionsQuery);
-  const { accounts } = accountsInfo.data;
-  for (const account of accounts) {
-    let userPositions = {};
-    const userAddr = account.id;
+  // 1. fetch users with AccountData
+  const accountsInfo = await apolloFetcher(usersQuery);
+  const { users } = accountsInfo.data;
+  const usersHealthRes = await multicallContract.callStatic.aggregate(
+    users.map((user) => ({
+      target: config.contracts.pool,
+      callData: poolInterface.encodeFunctionData("getUserAccountData", [
+        user.id,
+      ]),
+    }))
+  );
 
-    // init user positions
-    for (const position of account.positions) {
-      const idSplit = position.id.split("-");
-      const marketId = idSplit[1];
-      const positionType = idSplit[2];
+  // 2. generate unhealthy users
+  const unhealthyUsers = usersHealthRes[1]
+    .map((userHealth, ind) => {
+      const detailedInfo = poolInterface.decodeFunctionResult(
+        "getUserAccountData",
+        userHealth
+      );
 
-      if (_.isEmpty(userPositions[marketId])) {
-        const priceInd = priceInfos.find(
-          (priceInfo) => priceInfo.marketId == marketId
-        );
-        const selPrice = _.isEmpty(priceInd)
-          ? await getPairprice(position.market.oracle.oracleAddress)
-          : priceInd.price;
-        userPositions[marketId] = {
-          pair_price: selPrice,
-          lltv: BigNumber.from(position.market.lltv),
-        };
+      return {
+        user: users[ind].id,
+        healthy: BigNumber.from(detailedInfo.healthFactor),
+      };
+    })
+    .filter((userHealth) => userHealth.healthy.lt(constants.WeiPerEther));
+  console.log(unhealthyUsers);
 
-        if (_.isEmpty(priceInd)) {
-          priceInfos.push({
-            marketId,
-            price: selPrice,
-          });
-        }
-      }
+  // 3. fetch unhealthy users debt info
 
-      if (positionType == "BORROWER") {
-        let userBorrows = constants.Zero;
-        for (const item of position.borrows) {
-          userBorrows = userBorrows.add(BigNumber.from(item.amount));
-        }
+  // for (const account of accounts) {
+  //   let userPositions = {};
+  //   const userAddr = account.id;
 
-        let userRepays = constants.Zero;
-        for (const item of position.repays) {
-          userRepays = userRepays.add(BigNumber.from(item.amount));
-        }
+  //   // init user positions
+  //   for (const position of account.positions) {
+  //     const idSplit = position.id.split("-");
+  //     const marketId = idSplit[1];
+  //     const positionType = idSplit[2];
 
-        userPositions[marketId].borrow = userBorrows.gte(userRepays)
-          ? userBorrows.sub(userRepays)
-          : constants.Zero;
-      } else if (positionType == "COLLATERAL") {
-        userPositions[marketId].collateral = BigNumber.from(position.balance);
-      }
-    }
+  //     if (_.isEmpty(userPositions[marketId])) {
+  //       const priceInd = priceInfos.find(
+  //         (priceInfo) => priceInfo.marketId == marketId
+  //       );
+  //       const selPrice = _.isEmpty(priceInd)
+  //         ? await getPairprice(position.market.oracle.oracleAddress)
+  //         : priceInd.price;
+  //       userPositions[marketId] = {
+  //         pair_price: selPrice,
+  //         lltv: BigNumber.from(position.market.lltv),
+  //       };
 
-    // check health
-    if (!_.isEmpty(userPositions)) {
-      for (const marketId in userPositions) {
-        const userPosition = userPositions[marketId];
-        if (
-          _.isEmpty(userPosition.collateral) ||
-          _.isEmpty(userPosition.borrow)
-        )
-          continue;
+  //       if (_.isEmpty(priceInd)) {
+  //         priceInfos.push({
+  //           marketId,
+  //           price: selPrice,
+  //         });
+  //       }
+  //     }
 
-        if (
-          userPosition.collateral.gt(constants.Zero) &&
-          userPosition.borrow.gt(constants.Zero)
-        ) {
-          const borrowLimit = userPosition.collateral
-            .mul(userPosition.lltv)
-            .mul(oraclePriceScale)
-            .div(constants.WeiPerEther)
-            .div(userPosition.pair_price);
+  //     if (positionType == "BORROWER") {
+  //       let userBorrows = constants.Zero;
+  //       for (const item of position.borrows) {
+  //         userBorrows = userBorrows.add(BigNumber.from(item.amount));
+  //       }
 
-          if (borrowLimit.lt(userPosition.borrow)) {
-            unhealthPositions.push({
-              userAddr,
-            });
-          }
-        }
-      }
-    }
-  }
+  //       let userRepays = constants.Zero;
+  //       for (const item of position.repays) {
+  //         userRepays = userRepays.add(BigNumber.from(item.amount));
+  //       }
+
+  //       userPositions[marketId].borrow = userBorrows.gte(userRepays)
+  //         ? userBorrows.sub(userRepays)
+  //         : constants.Zero;
+  //     } else if (positionType == "COLLATERAL") {
+  //       userPositions[marketId].collateral = BigNumber.from(position.balance);
+  //     }
+  //   }
+
+  //   // check health
+  //   if (!_.isEmpty(userPositions)) {
+  //     for (const marketId in userPositions) {
+  //       const userPosition = userPositions[marketId];
+  //       if (
+  //         _.isEmpty(userPosition.collateral) ||
+  //         _.isEmpty(userPosition.borrow)
+  //       )
+  //         continue;
+
+  //       if (
+  //         userPosition.collateral.gt(constants.Zero) &&
+  //         userPosition.borrow.gt(constants.Zero)
+  //       ) {
+  //         const borrowLimit = userPosition.collateral
+  //           .mul(userPosition.lltv)
+  //           .mul(oraclePriceScale)
+  //           .div(constants.WeiPerEther)
+  //           .div(userPosition.pair_price);
+
+  //         if (borrowLimit.lt(userPosition.borrow)) {
+  //           unhealthPositions.push({
+  //             userAddr,
+  //           });
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
 }
 
 main()
--- query.js
+++ query.js
@@ -1,35 +1,13 @@
 const gql = require("graphql-tag");
 
-const positionsQuery = gql`
+const usersQuery = gql`
   {
-    accounts(first: 1000) {
+    users {
       id
-      positions(first: 100) {
-        id
-        market {
-          lltv
-          inputToken {
-            id
-          }
-          borrowedToken {
-            id
-          }
-          oracle {
-            oracleAddress
-          }
-        }
-        balance
-        borrows {
-          amount
-        }
-        repays {
-          amount
-        }
-      }
     }
   }
 `;
 
 module.exports = {
-  positionsQuery,
+  usersQuery,
 };
