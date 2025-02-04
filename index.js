237284b10da29aa0ad8bf6b889a276af44abafac
mmsys34 <leon@more.markets>
2025-02-04 17:10:37 +0700

    feat: update for univ3 liquidate

--- index.js
+++ index.js
@@ -15,13 +15,16 @@ const { usersQuery } = require("./query.js");
 
 const config = require("./config.json");
 const PoolAbi = require("./abis/Pool.json");
+const MTokenAbi = require("./abis/MToken.json");
 const OracleAbi = require("./abis/OracleAbi.json");
 const MulticallAbi = require("./abis/MulticallAbi.json");
+const LiquidationAbi = require("./abis/Liquidation.json");
 
 const provider = new providers.JsonRpcProvider(config.rpc_url);
 
 // interfaces
 const poolInterface = new utils.Interface(PoolAbi);
+const mTokenInterface = new utils.Interface(MTokenAbi);
 
 const multicallContract = new Contract(
   config.contracts.multicall,
@@ -43,26 +46,27 @@ const apolloFetcher = async (query) => {
   });
 };
 
-const getPairprice = async (oracle) => {
-  const oracleContract = new Contract(oracle, OracleAbi, provider);
-  return await oracleContract.price();
-};
+const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
 // exports.handler = async (event) => {
 async function main() {
-  let priceInfos = [];
-  let unhealthPositions = [];
-
   // 1. fetch users with AccountData
   const accountsInfo = await apolloFetcher(usersQuery);
   const { users } = accountsInfo.data;
+
+  let usersHealthReq = [];
+  config.pools.map((pool) => {
+    users.map((user) => {
+      usersHealthReq.push({
+        target: pool,
+        callData: poolInterface.encodeFunctionData("getUserAccountData", [
+          user.id,
+        ]),
+      });
+    });
+  });
   const usersHealthRes = await multicallContract.callStatic.aggregate(
-    users.map((user) => ({
-      target: config.contracts.pool,
-      callData: poolInterface.encodeFunctionData("getUserAccountData", [
-        user.id,
-      ]),
-    }))
+    usersHealthReq
   );
 
   // 2. generate unhealthy users
@@ -73,94 +77,135 @@ async function main() {
         userHealth
       );
 
+      const userInd = ind % users.length;
+      const poolInd = Math.floor(ind / users.length);
       return {
-        user: users[ind].id,
+        user: users[userInd].id,
+        pool: config.pools[poolInd],
         healthy: BigNumber.from(detailedInfo.healthFactor),
       };
     })
-    .filter((userHealth) => userHealth.healthy.lt(constants.WeiPerEther));
+    .filter(
+      (userHealth) =>
+        userHealth.healthy.lt(constants.WeiPerEther) && userHealth.healthy.gt(0)
+    );
   console.log(unhealthyUsers);
 
   // 3. fetch unhealthy users debt info
+  const liquidator = new Wallet(config.liquidator_key, provider);
+  for (const unhealthyUser of unhealthyUsers) {
+    let collateralAsset = "";
+    let debtAsset = "";
 
-  // for (const account of accounts) {
-  //   let userPositions = {};
-  //   const userAddr = account.id;
+    const botInfo = config.bots[unhealthyUser.pool];
+    let mTokenRequest = [];
+    botInfo.mtokens.map((mToken) => {
+      mTokenRequest.push({
+        target: mToken,
+        callData: mTokenInterface.encodeFunctionData("balanceOf", [
+          unhealthyUser.user,
+        ]),
+      });
 
-  //   // init user positions
-  //   for (const position of account.positions) {
-  //     const idSplit = position.id.split("-");
-  //     const marketId = idSplit[1];
-  //     const positionType = idSplit[2];
+      mTokenRequest.push({
+        target: mToken,
+        callData: mTokenInterface.encodeFunctionData(
+          "UNDERLYING_ASSET_ADDRESS",
+          []
+        ),
+      });
+    });
 
-  //     if (_.isEmpty(userPositions[marketId])) {
-  //       const priceInd = priceInfos.find(
-  //         (priceInfo) => priceInfo.marketId == marketId
-  //       );
-  //       const selPrice = _.isEmpty(priceInd)
-  //         ? await getPairprice(position.market.oracle.oracleAddress)
-  //         : priceInd.price;
-  //       userPositions[marketId] = {
-  //         pair_price: selPrice,
-  //         lltv: BigNumber.from(position.market.lltv),
-  //       };
+    botInfo.dTokens.map((dToken) => {
+      mTokenRequest.push({
+        target: dToken,
+        callData: mTokenInterface.encodeFunctionData("balanceOf", [
+          unhealthyUser.user,
+        ]),
+      });
 
-  //       if (_.isEmpty(priceInd)) {
-  //         priceInfos.push({
-  //           marketId,
-  //           price: selPrice,
-  //         });
-  //       }
-  //     }
+      mTokenRequest.push({
+        target: dToken,
+        callData: mTokenInterface.encodeFunctionData(
+          "UNDERLYING_ASSET_ADDRESS",
+          []
+        ),
+      });
+    });
+    const tokenRes = await multicallContract.callStatic.aggregate(
+      mTokenRequest
+    );
 
-  //     if (positionType == "BORROWER") {
-  //       let userBorrows = constants.Zero;
-  //       for (const item of position.borrows) {
-  //         userBorrows = userBorrows.add(BigNumber.from(item.amount));
-  //       }
+    let mInfos = [];
+    let dInfos = [];
+    const tokenInfos = tokenRes[1].map((tokenRes, ind) => ({
+      info: mTokenInterface.decodeFunctionResult(
+        ind % 2 == 0 ? "balanceOf" : "UNDERLYING_ASSET_ADDRESS",
+        tokenRes
+      ),
+    }));
+    for (let ii = 0; ii < tokenInfos.length - 1; ii++) {
+      const selInd = ii % 2;
+      if (selInd == 0) {
+        const detailedInfo = tokenInfos[ii].info[0];
+        if (detailedInfo.gt(0)) {
+          if (ii < botInfo.mtokens.length * 2) {
+            mInfos.push({
+              mtoken: botInfo.mtokens[Math.floor(ii / 2)],
+              token: tokenInfos[ii + 1].info,
+              amount: BigNumber.from(detailedInfo),
+            });
+          } else {
+            dInfos.push({
+              token: tokenInfos[ii + 1].info,
+              amount: BigNumber.from(detailedInfo),
+            });
+          }
+        }
+      }
+    }
 
-  //       let userRepays = constants.Zero;
-  //       for (const item of position.repays) {
-  //         userRepays = userRepays.add(BigNumber.from(item.amount));
-  //       }
+    // console.log(mInfos, dInfos, unhealthyUser.user);
+    if (mInfos.length > 0 && dInfos.length > 0) {
+      collateralAsset = mInfos[0].token[0];
+      debtAsset = dInfos[0].token[0];
 
-  //       userPositions[marketId].borrow = userBorrows.gte(userRepays)
-  //         ? userBorrows.sub(userRepays)
-  //         : constants.Zero;
-  //     } else if (positionType == "COLLATERAL") {
-  //       userPositions[marketId].collateral = BigNumber.from(position.balance);
-  //     }
-  //   }
+      const collateralContract = new Contract(
+        collateralAsset,
+        MTokenAbi,
+        provider
+      );
+      const collBal = await collateralContract.balanceOf(mInfos[0].mtoken);
+      const calBal = dInfos[0].amount.mul(1100).div(1e3);
+      const checkedBal = calBal > collBal ? collBal : calBal;
+      const cover = calBal > collBal ? collBal : constants.MaxUint256;
 
-  //   // check health
-  //   if (!_.isEmpty(userPositions)) {
-  //     for (const marketId in userPositions) {
-  //       const userPosition = userPositions[marketId];
-  //       if (
-  //         _.isEmpty(userPosition.collateral) ||
-  //         _.isEmpty(userPosition.borrow)
-  //       )
-  //         continue;
+      const botContract = new Contract(
+        config.bots[unhealthyUser.pool].bot,
+        LiquidationAbi,
+        provider
+      );
+      const lParam = {
+        collateralAsset,
+        debtAsset,
+        user: unhealthyUser.user,
+        amount: checkedBal,
+        transferAmount: 0,
+        debtToCover: cover,
+      };
+      const sParam = {
+        receiver: liquidator.address,
+        swapRouter: config.contracts.router,
+        path1: [collateralAsset, debtAsset],
+        path2: [debtAsset, config.contracts.wflow],
+      };
+      console.log(lParam, sParam);
 
-  //       if (
-  //         userPosition.collateral.gt(constants.Zero) &&
-  //         userPosition.borrow.gt(constants.Zero)
-  //       ) {
-  //         const borrowLimit = userPosition.collateral
-  //           .mul(userPosition.lltv)
-  //           .mul(oraclePriceScale)
-  //           .div(constants.WeiPerEther)
-  //           .div(userPosition.pair_price);
-
-  //         if (borrowLimit.lt(userPosition.borrow)) {
-  //           unhealthPositions.push({
-  //             userAddr,
-  //           });
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
+      const tx = await botContract.connect(liquidator).execute(lParam, sParam);
+      await tx.wait();
+      await sleep(3000);
+    }
+  }
 }
 
 main()
